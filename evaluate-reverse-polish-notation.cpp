ea67fc0dd055de604d0835945240bf24a036d4d1
Shobhit <smartyshobhit@gmail.com>
2014-03-01 22:25:42 -0800

    erpn

---
+++ evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int evalRPN(vector<string> &tokens) {
+        stack<int> st;
+        for(int i = 0; i < tokens.size(); i ++)
+        {
+            if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/")
+            {
+                int num1 = st.top();
+                st.pop();
+                int num2 = st.top();
+                st.pop();
+                if(tokens[i] == "+")
+                    st.push(num1+num2);
+                if(tokens[i] == "*")
+                    st.push(num1*num2);
+                if(tokens[i] == "-")
+                    st.push(num2-num1);
+                if(tokens[i] == "/")
+                    st.push(num2/num1);
+            }
+            else
+            {
+                st.push(stringToInt(tokens[i]));
+            }
+        }
+        
+        return st.top();
+    }
+
+private :
+    int stringToInt(string str)
+    {
+        return atoi(str.c_str());
+    }
+};
