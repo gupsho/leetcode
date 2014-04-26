a185f6d1dd3089b3af2d402359ed7ff30e867e41
Shobhit <smartyshobhit@gmail.com>
2014-04-26 13:07:46 -0700

    best-time-to-buy-and-sell-stock-ii.cpp

--- cpp/best-time-to-buy-and-sell-stock-ii.cpp
+++ cpp/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-        int profit = 0;
         if(prices.size() <= 1)
-            return profit;
-            
-        int buyPrice = prices[0];
-        for(int i = 1; i < prices.size(); i ++)
+            return 0;
+        int profit = 0, buy = prices[0];
+        for(int i = 1; i < prices.size(); i++)
         {
-            if(buyPrice > prices[i])
-                buyPrice = prices[i];
-            else
-            {
-                profit += prices[i] - buyPrice;
-                buyPrice = prices[i];
-            }
+            if(buy < prices[i])
+                profit += (prices[i] - buy);
+            buy = prices[i];
         }
         return profit;
     }
